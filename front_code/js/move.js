1e1ebb1aca1962f4b9b812f3b5b362be9aa30bf7
water-wine <923460842@qq.com>
2022-11-23 22:23:29 +0800

    移动

--- front_code/js/move.js
+++ front_code/js/move.js
@@ -1,21 +1,34 @@
-function animate(obj, target, num, callback) {
+function animate1(obj, target, num, callback) {
     clearInterval(obj.timer);
     obj.timer = setInterval(function () {
-        // var stepX = num*(-21);
-        // var stepY = num*(-12);
-        if (obj.offsetLeft == target) {
+        if (obj.offsetLeft <= target) {
             clearInterval(obj.timer);
-            // callback();
             console.log(target);
             console.log(player1.offsetLeft);
             console.log(player1.offsetTop);
         }
         else {
-            // obj.style.left = obj.offsetLeft + stepX + 'px';
-            // obj.style.bottom = obj.offsetTop + stepY +'px';
-            obj.style.left = obj.style.left + 1 + 'px';
-            obj.style.bottom = obj.style.bottom + 0.58 +'px';
+            var top = obj.offsetTop - 67;
+            obj.style.left = obj.offsetLeft - 2 + 'px';
+            obj.style.top = top - 1 +'px';
         }
-    },300)
+    },100)
+
+}
+function animate2(obj, target, num, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        if (obj.offsetLeft <= target) {
+            clearInterval(obj.timer);
+            console.log(target);
+            console.log(player1.offsetLeft);
+            console.log(player1.offsetTop);
+        }
+        else {
+            var top = obj.offsetTop - 135;
+            obj.style.left = obj.offsetLeft - 2 + 'px';
+            obj.style.top = top - 1 +'px';
+        }
+    },100)
 
 }
