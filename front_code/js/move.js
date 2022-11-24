07a06fea23545363405e19b42dffc79b336cef51
water-wine <923460842@qq.com>
2022-11-24 18:48:05 +0800

    倒计时

--- front_code/js/move.js
+++ front_code/js/move.js
@@ -10,9 +10,9 @@ function animate1(obj, target, num, callback) {
         else {
             var top = obj.offsetTop - 67;
             obj.style.left = obj.offsetLeft - 2 + 'px';
-            obj.style.top = top - 1 +'px';
+            obj.style.top = top - 1 + 'px';
         }
-    },100)
+    }, 100)
 
 }
 function animate2(obj, target, num, callback) {
@@ -27,8 +27,26 @@ function animate2(obj, target, num, callback) {
         else {
             var top = obj.offsetTop - 135;
             obj.style.left = obj.offsetLeft - 2 + 'px';
-            obj.style.top = top - 1 +'px';
+            obj.style.top = top - 1 + 'px';
         }
-    },100)
+    }, 100)
 
+}
+
+function clock(obj) {
+    var time = 10;
+    var timer = setInterval(function () {
+        if (time == 0) {
+            clearInterval(timer);
+            obj.innerHTML = '倒计时';
+        }
+        else {
+            obj.innerHTML = time;
+            time--;
+        }
+    }, 1000);
+}
+
+function getnum() {
+    return Math.floor(Math.random()*6) + 1;
 }
