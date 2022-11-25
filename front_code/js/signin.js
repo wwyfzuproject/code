45facc224d5bd2e2a290c4c2c9b1d1a449d8babb
kidxxid <3267982421@qq.com>
2022-11-25 16:35:47 +0800

    signin.js  2.0

--- front_code/js/signin.js
+++ front_code/js/signin.js
@@ -1,15 +1,33 @@
 $(function(){
-    $("#signinForm").on("submit",function(data){
-        var formdata=$("#signinForm").serialize()
+    // 登录
+    $("#signinForm").on("submit",function(e){
+        e.preventDefault();
+        var formdata1=$("#signinForm").serialize();
         
         $.ajax({
-            type:'post',
-            url:'http://httpbin.org/post',
-            data:{formdata},
+            type:'get',
+            url:'http://httpbin.org/get',
+            data:{formdata1},
             success:function(resp){
                 console.log(resp);
             }
-        })
+        });
 
-    })
-})
+    });
+    
+    // 注册
+    $("#signupForm").on("submit",function(e){
+        e.preventDefault();
+        var formdata2=$("#signupForm").serialize();
+        
+        $.ajax({
+            type:'get',
+            url:'http://httpbin.org/get',
+            data:{formdata2},
+            success:function(resp){
+                console.log(resp);
+            }
+        });
+
+    });
+});
