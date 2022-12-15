45590843ad08c284f3fba62a92c346268b8821a8
phllllllll <2505629270@qq.com>
2022-12-15 14:52:50 +0800

    签到分享js

--- front_code/js/share&sign.js
+++ front_code/js/share&sign.js
@@ -8,11 +8,11 @@ $(function(){
             type:"get",
             dataType:"json",
             data:objs,
-            success:function(resp){
-                console.log(resp);
+            success:function(){
+                SignSuccess();
             },
             error:function(){
-                alert("签到失败，请重试！");
+                SignFailure();
             }
         });
     });
@@ -23,15 +23,58 @@ $(function(){
         $.ajax({
             url:"",
             type:"get",
-            dataType:"json",
-            data:objr,
             success:function(resp){
-                console.log(resp);
+                ShareSuccess();
             },
             error:function(){
-                alert("分享失败,请重试！");
+                ShareFailure();
             }
         });
     });
 
 });
+
+function SignSuccess(){
+    var tan1 = document.getElementById('tan1');
+    var qiandao = document.querySelector(".signin");
+    var close1 = document.querySelector(".close");
+    tan1.style.display = "none";
+    tan2.style.display = "none";
+    qiandao.onclick = function () {
+        tan1.style.display = "block";
+    }
+    close1.onclick = function () {
+        tan1.style.display = "none";
+
+    }
+};
+function SignFailure(){
+    alert("签到失败！请重试！");
+};
+function ShareSuccess(){
+    var close2 = document.querySelector(".close2")
+    var tan2 = document.querySelector(".tan2");
+    var share = document.querySelector(".share");
+    var copy = document.querySelector(".copy");
+    var copysuccess = document.querySelector(".copysuccess");
+    tan1.style.display = "none";
+    tan2.style.display = "none";
+    copysuccess.style.display = "none";
+
+    share.onclick = function () {
+        tan2.style.display = "block";
+    }
+    close2.onclick = function () {
+        tan2.style.display = "none";
+    }
+    copy.onclick=function(){
+        copysuccess.style.display = "block";
+    }
+    setTimeout(function(){
+        copysuccess.style.display="none";
+    },2000)
+
+};
+function ShareFailure(){
+    alert("分享失败！请重试！");
+};
