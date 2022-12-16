aca1ebe14e6f2046527fee8e1ad23f7a509efa83
kidxxid <3267982421@qq.com>
2022-12-16 20:49:32 +0800

    小修改

--- front_code/js/share&sign.js
+++ front_code/js/share&sign.js
@@ -1,20 +1,81 @@
-$(function(){
-    // 签到按钮
-    var sign = document.querySelector(".signin");
-    sign.onclick = function(){
-        var objs={};
-        objs['msg'] = 'sign';
-        $.ajax({
-            url:"",
-            type:"get",
-            dataType:"json",
-            data:JSON.stringify(objs),
-            success:SignSuccess(),
-            error:SignFailure()
-        });
-    };
+// $(function(){
+//     // 签到按钮
+//     var sign = document.querySelector(".signin");
+//     sign.onclick = function(){
+//         var objs={};
+//         objs['msg'] = 'sign';
+//         $.ajax({
+//             url:"",
+//             type:"get",
+//             dataType:"json",
+//             data:JSON.stringify(objs),
+//             success:SignSuccess(),
+//             error:SignFailure()
+//         });
+//     };
     
-    // 分享按钮
+//     // 分享按钮
+//     var share = document.querySelector(".share");
+//     share.onclick = function (){
+//         var close2 = document.querySelector(".close2")
+//         var tan2 = document.querySelector(".tan2");
+//         var share = document.querySelector(".share");    
+//         share.onclick = function () {
+//             tan2.style.display = "block";
+//         }
+//         close2.onclick = function () {
+//             tan2.style.display = "none";
+//         }
+//     };
+
+
+//     function SignSuccess(){
+//         var tan1 = document.getElementById('tan1');
+//         var qiandao = document.querySelector(".signin");
+//         var close1 = document.querySelector(".close");
+//         // tan1.style.display = "none";
+//         qiandao.onclick = function () {
+//             tan1.style.display = "block";
+//         }
+//         close1.onclick = function () {
+//             tan1.style.display = "none";
+    
+//         }
+//     };
+//     function SignFailure(){
+//         var qiandao = document.querySelector(".signin");
+//         qiandao.onclick = function () {
+//             alert("签到失败！请重试！");
+//         }
+//     };
+    
+//     function ShareFailure(){ 
+//         var share = document.querySelector(".share");   
+//         share.onclick = function () {
+//             alert("分享失败！请重试！");
+//         }
+       
+//     };
+    
+// });
+
+
+function SignSuccess(){
+    var tan1 = document.getElementById('tan1');
+    var qiandao = document.querySelector(".signin");
+    var close1 = document.querySelector(".close");
+    // tan1.style.display = "none";
+    qiandao.onclick = function () {
+        tan1.style.display = "block";
+    }
+    close1.onclick = function () {
+        tan1.style.display = "none";
+
+    }
+};
+
+$(function(){
+    SignSuccess();
     var share = document.querySelector(".share");
     share.onclick = function (){
         var close2 = document.querySelector(".close2")
@@ -27,62 +88,4 @@ $(function(){
             tan2.style.display = "none";
         }
     };
-
-
-    function SignSuccess(){
-        var tan1 = document.getElementById('tan1');
-        var qiandao = document.querySelector(".signin");
-        var close1 = document.querySelector(".close");
-        // tan1.style.display = "none";
-        qiandao.onclick = function () {
-            tan1.style.display = "block";
-        }
-        close1.onclick = function () {
-            tan1.style.display = "none";
-    
-        }
-    };
-    function SignFailure(){
-        var qiandao = document.querySelector(".signin");
-        qiandao.onclick = function () {
-            alert("签到失败！请重试！");
-        }
-    };
-    
-    function ShareFailure(){ 
-        var share = document.querySelector(".share");   
-        share.onclick = function () {
-            alert("分享失败！请重试！");
-        }
-       
-    };
-    
 });
-
-
-//     function ShareSuccess(){
-//         var close2 = document.querySelector(".close2")
-//         var tan2 = document.querySelector(".tan2");
-//         var share = document.querySelector(".share");
-//         var copy = document.querySelector(".copy");
-//         var copysuccess = document.querySelector(".copysuccess");
-//         // tan2.style.display = "none";
-//         // copysuccess.style.display = "none";
-    
-//         share.onclick = function () {
-//             tan2.style.display = "block";
-//         }
-//         close2.onclick = function () {
-//             tan2.style.display = "none";
-//         }
-//         // copy.onclick=function(){
-//         //     copysuccess.style.display = "block";
-//         //     setTimeout(function(){
-//         //         copysuccess.style.display="none";
-//         //     },2000)
-//         // }
-//     }
-
-// $(function(){
-//     ShareSuccess();
-// });
