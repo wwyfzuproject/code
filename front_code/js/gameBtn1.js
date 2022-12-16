6d3654b432ec5afb4398676ed78072ebf471ce2c
kidxxid <3267982421@qq.com>
2022-12-16 20:07:01 +0800

    startbtn

---
+++ front_code/js/gameBtn1.js
@@ -0,0 +1,19 @@
+// $(function(){
+//     var start = document.querySelector(".accept-deduct-gold");
+//     start.onclick = function(){
+//         var objm = {};
+//         objm['msg'] = 'money';
+//         $.ajax({
+//             url:"",
+//             type:"get",
+//             dataType:"json",
+//             data:JSON.stringify(objm),
+//             success:function(){
+
+//             },
+//             error:function(){
+//                 alert("加载失败！请重试！");
+//             }
+//         });
+//     };
+// });
